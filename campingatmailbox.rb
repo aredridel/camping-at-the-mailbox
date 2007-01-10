9636f146e40ae616f38674e4d308dd5ffeeefcff
Aredridel <aredridel@nbtsc.org>
2007-01-10 14:21:25 -0700

    Handle https URLs too
    darcs-hash:20070110212125-92d64-aca51403d061c77feac759cd4c9e6531667830d1.gz

--- campingatmailbox.rb
+++ campingatmailbox.rb
@@ -1281,7 +1281,7 @@ module CampingAtMailbox
 				case structure.subtype
 				when 'PLAIN'
 					pre do
-						capture { WordWrapper.wrap(part).gsub(%r{(http://[^[:space:]]+)}) { |m| "<a href='#{$1}' target='_new'>#{$1}</a>" } }
+						capture { WordWrapper.wrap(part).gsub(%r{(https?://[^[:space:]]+)}) { |m| "<a href='#{$1}' target='_new'>#{$1}</a>" } }
 					end
 				when 'HTML'
 					div.htmlmessage do
